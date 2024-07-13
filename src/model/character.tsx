61ee853c82f9ca0b2f38da28b502b9dc40beb823
NicoCLM <nc090202@gmail.com>
2024-07-13 02:09:53 -0500

    Fetch from characters Working

---
+++ src/model/character.tsx
@@ -0,0 +1,53 @@
+interface locationChar{
+    name: string;
+    url: string;
+}
+
+interface originChar{
+    name: string;
+    url: string
+}
+
+interface Character {
+    id:number;
+    name:string;
+    status:string;
+    species:string;
+    type:string;
+    gender:string;
+    origin:originChar;
+    location:locationChar;
+    image:string;
+    episode:string [];
+    url:string;
+    created:string;
+};
+
+interface Location {
+    id:number;
+    name:string;
+    type:string;
+    dimension:string;
+    residents:string[];
+    url:string;
+    created:string;
+};
+
+interface Episode {
+    id:number;
+    name:string;
+    air_date:string;
+    episode:string;
+    characters:string[];
+    url:string;
+    created:string;
+};
+
+
+
+export const getCharacters = async(): Promise<any[]> =>{
+    const response = await fetch(`https://rickandmortyapi.com/api/character`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+};
